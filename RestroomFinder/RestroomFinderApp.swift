ebcd7804f62693f628b3f748c5e21d509017f7c3
superkookai <weerawut@live.com>
2025-04-22 10:21:06 +0700

    Initial Commit

---
+++ RestroomFinder/RestroomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestroomFinderApp.swift
+//  RestroomFinder
+//
+//  Created by Weerawut Chaiyasomboon on 22/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct RestroomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
